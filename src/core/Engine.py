bc13b38fc5d8827f74c59a15b0d04ad5a2149e69
Matheus Menck <matheushmenck@gmail.com>
2026-03-29 11:17:41 -0300

    Refatorando o código, limpando a engine.py

--- src/core/Engine.py
+++ src/core/Engine.py
@@ -4,6 +4,7 @@ from src.mapa.Grid import Grid
 from src.core.Camera import Camera
 from src.ui.Hud import Hud
 from src.core.Construcao import Construcao
+from src.core.SistemaConstruir import SistemaConstruir
 
 class Engine:
     def __init__(self, tela):
@@ -22,12 +23,7 @@ class Engine:
         largura_janela, altura_janela = self.screen.get_size()
         self.hud = Hud(largura_janela, altura_janela)
         
-        # parte das contruções
-        self.construcoes = []  
-        self.modo_construcao = False
-        self.tipo_construcao_atual = 'residencial'
-        self.tipos_disponiveis = ['residencial', 'serraria', 'mina', 'pesca']
-        self.indice_tipo_atual = 0
+        self.sistema_construir = SistemaConstruir(self.cols, self.lins, self.grid, self.camera, self.screen)
 
     def start(self):
         while self.running:
@@ -45,23 +41,7 @@ class Engine:
                     if event.key == pygame.K_h:
                         self.hud.toggle_controles()
                     
-                    # alternar  entre os modos
-                    if event.key == pygame.K_b:
-                        self.modo_construcao = not self.modo_construcao
-                        print(f"Modo {'CONSTRUÇÃO' if self.modo_construcao else 'SELEÇÃO'}")
-                    
-                    if event.key == pygame.K_1:
-                        self.tipo_construcao_atual = 'residencial'
-                        self.indice_tipo_atual = self.tipos_disponiveis.index('residencial')
-                    if event.key == pygame.K_2:
-                        self.tipo_construcao_atual = 'serraria'
-                        self.indice_tipo_atual = self.tipos_disponiveis.index('serraria')
-                    if event.key == pygame.K_3:
-                        self.tipo_construcao_atual = 'mina'
-                        self.indice_tipo_atual = self.tipos_disponiveis.index('mina')
-                    if event.key == pygame.K_4:
-                        self.tipo_construcao_atual = 'pesca'
-                        self.indice_tipo_atual = self.tipos_disponiveis.index('pesca')
+                    self.sistema_construir.handle_keydown(event)
                     
                 
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -69,20 +49,10 @@ class Engine:
                         mouse_pos = pygame.mouse.get_pos()
                         pos_grid = self.grid.tela_para_grid(mouse_pos, self.camera.x, self.camera.y)
                         
-                        if pos_grid:
-                            if self.modo_construcao:
-                                if not self.tem_construcao(pos_grid):
-                                    self.construir(pos_grid)
-                                else:
-                                    self.grid.selecionar_celula(mouse_pos, self.camera.x, self.camera.y)
-                            else:
-                                self.grid.selecionar_celula(mouse_pos, self.camera.x, self.camera.y)
-                    
-                    elif event.button == 3: 
-                        mouse_pos = pygame.mouse.get_pos()
-                        pos_grid = self.grid.tela_para_grid(mouse_pos, self.camera.x, self.camera.y)
-                        if pos_grid:
-                            self.remover_construcao(pos_grid)
+                        self.sistema_construir.handle_mousebuttondown(event)
+        
+                    elif event.button == 3:
+                        self.sistema_construir.handle_mousebuttondown(event)
 
                 if event.type == pygame.MOUSEWHEEL:
                     if event.y > 0:
@@ -93,8 +63,7 @@ class Engine:
             if self.camera.atualizar_zoom(largura_janela, altura_janela):
                 self.mapa = Mapa(id_mapa=4, tile_size=int(self.camera.tile_size))
                 self.grid = Grid(colunas=self.cols, linhas=self.lins, tile_size=int(self.camera.tile_size))
-                for construcao in self.construcoes:
-                    construcao.tile_size = int(self.camera.tile_size)
+                self.sistema_construir.atualizar_tile_size(int(self.camera.tile_size))
 
             self.camera.mover_por_teclado(largura_janela, altura_janela)
             self.camera.mover_por_mouse(largura_janela, altura_janela)
@@ -103,74 +72,10 @@ class Engine:
             # Renderização
             self.screen.fill((20, 20, 20))
             self.mapa.draw(self.screen, self.camera.x, self.camera.y)
-            self.desenhar_construcoes()
+            self.sistema_construir.update()
             if self.mostrar_grid:
                 self.grid.draw_debug(self.screen, self.camera.x, self.camera.y)
-            if self.modo_construcao:
-                self.desenhar_previa_construcao()
             self.hud.desenhar(self.screen)
 
             pygame.display.flip()
             self.clock.tick(60)
-
-        
-    # metodos do menu de contruir
-    def tem_construcao(self, pos_grid):
-        for construcao in self.construcoes:
-            if construcao.posicao == pos_grid:
-                return True
-        return False
-    
-    def get_construcao(self, pos_grid):
-        for construcao in self.construcoes:
-            if construcao.posicao == pos_grid:
-                return construcao
-        return None
-    
-    def construir(self, pos_grid):
-        if 0 <= pos_grid[0] < self.cols and 0 <= pos_grid[1] < self.lins:
-            if not self.tem_construcao(pos_grid):
-                nova_construcao = Construcao(
-                    self.tipo_construcao_atual,
-                    pos_grid,
-                    int(self.camera.tile_size)
-                )
-                self.construcoes.append(nova_construcao)
-                return True 
-        return False
-    
-    def remover_construcao(self, pos_grid):
-        construcao = self.get_construcao(pos_grid)
-        if construcao:
-            self.construcoes.remove(construcao)
-            print(f"")
-            return True
-        return False
-    
-    def desenhar_construcoes(self):
-        for construcao in self.construcoes:
-            construcao.desenhar(self.screen, self.camera.x, self.camera.y)
-    
-    def desenhar_previa_construcao(self):
-        mouse_pos = pygame.mouse.get_pos()
-        pos_grid = self.grid.tela_para_grid(mouse_pos, self.camera.x, self.camera.y)
-        
-        if pos_grid and not self.tem_construcao(pos_grid):
-            x = (pos_grid[0] * self.camera.tile_size) - self.camera.x
-            y = (pos_grid[1] * self.camera.tile_size) - self.camera.y
-            
-            surface = pygame.Surface((self.camera.tile_size, self.camera.tile_size), pygame.SRCALPHA)
-            
-            # Cores da previa
-            cores_previa = {
-                'residencial': (255, 0, 0, 128),       
-                'serraria': (200, 150, 100, 128),      
-                'mina': (137, 137, 137, 128),          
-                'pesca': (100, 150, 200, 128)           
-            }
-            
-            cor = cores_previa.get(self.tipo_construcao_atual, (200, 200, 200, 128))
-            surface.fill(cor)
-            pygame.draw.rect(surface, (255, 255, 0), (0, 0, self.camera.tile_size, self.camera.tile_size), 2)
-            
-            self.screen.blit(surface, (x, y))
