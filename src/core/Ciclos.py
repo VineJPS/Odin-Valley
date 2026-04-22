14344350272e32e55186e13484967fcf2d5eb5e0
VineJPS <vinicius108sousa@gmail.com>
2026-04-22 17:22:10 -0300

    Adicionado o sistema de ciclos

---
+++ src/core/Ciclos.py
@@ -0,0 +1,93 @@
+import pygame
+
+class Ciclos:
+    def __init__(self):
+        self.tempo_ficticio = 0.0  # Tempo total pausável em ms (para dias)
+        self.tempo_decorrido_ciclo = 0.0
+        self.duracao_dia = 18000  # 5 minutos em ms
+        self.duracao_noite = 18000  # 3 minutos em ms
+        self.estado_atual = 'dia'
+        self.progresso = 0.0
+        self.dia_atual = 1
+        self._duracao_atual = self.duracao_dia
+        self.cor_ceu_dia = (135, 206, 235)
+        self.cor_ceu_noite = (25, 25, 112)
+        self.cor_ceu = self.cor_ceu_dia
+
+    def update(self, dt, paused):
+        if paused:
+            return
+
+        self.tempo_ficticio += dt
+        hora_decimal = self.get_hora_decimal()
+        self.progresso = (abs(hora_decimal - 12) / 6) ** 2 * 0.8  # Pico escuro meia-noite, suave transição
+
+        if 6 <= hora_decimal < 18:
+            self.estado_atual = 'dia'
+            self.cor_ceu = self.cor_ceu_dia
+            self.progresso = 0.0
+        else:
+            self.estado_atual = 'noite'
+            self.cor_ceu = self.cor_ceu_noite
+            self.progresso = ((hora_decimal - 18) % 24) / 12  # 0-1 suave noite
+
+    def get_hora_decimal(self):
+        ciclo_total = self.duracao_dia + self.duracao_noite
+        fracao_dia = (self.tempo_ficticio % ciclo_total) / ciclo_total
+        return fracao_dia * 24
+
+
+
+    def _alternar_ciclo(self):
+        if self.estado_atual == 'dia':
+            self.estado_atual = 'noite'
+            self._duracao_atual = self.duracao_noite
+            self.cor_ceu = self.cor_ceu_noite
+        else:
+            self.estado_atual = 'dia'
+            self._duracao_atual = self.duracao_dia
+            self.cor_ceu = self.cor_ceu_dia
+            self.dia_atual += 1
+
+    def get_estado(self):
+        return self.estado_atual
+
+    def get_progresso(self):
+        return self.progresso
+
+    def get_cor_ceu(self):
+        return self.cor_ceu
+
+    def is_noite(self):
+        return self.estado_atual == 'noite'
+
+    def get_alpha_sombra(self):
+        return self.progresso * 0.7 if self.is_noite() else 0.0
+
+    def get_tempo_hh_mm(self):
+        ciclo_total = self.duracao_dia + self.duracao_noite
+        fracao_dia = self.tempo_ficticio / ciclo_total
+        dias_totais = int(fracao_dia)
+        minutos_do_dia = (fracao_dia % 1) * 24 * 60
+        horas = int(minutos_do_dia // 60)
+        minutos = int(minutos_do_dia % 60)
+        self.dia_atual = dias_totais + 1
+        return f"{horas:02d}:{minutos:02d}"
+
+    def get_save_data(self):
+        return {
+            'tempo_ficticio': self.tempo_ficticio,
+            'dia_atual': self.dia_atual,
+            'estado_atual': self.estado_atual,
+            'tempo_decorrido_ciclo': self.tempo_decorrido_ciclo
+        }
+
+    def load_data(self, data):
+        self.tempo_ficticio = data.get('tempo_ficticio', 0.0)
+        self.dia_atual = data.get('dia_atual', 1)
+        self.estado_atual = data.get('estado_atual', 'dia')
+        self.tempo_decorrido_ciclo = data.get('tempo_decorrido_ciclo', 0.0)
+        self._duracao_atual = self.duracao_noite if self.estado_atual == 'noite' else self.duracao_dia
+        self.progresso = self.tempo_decorrido_ciclo / self._duracao_atual
+        self.cor_ceu = self.cor_ceu_noite if self.estado_atual == 'noite' else self.cor_ceu_dia
+
